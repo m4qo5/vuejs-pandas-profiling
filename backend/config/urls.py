34566e78b8f3798e5618630c934e78685d75f5a5
SlavaPasedko <mefi-100@tut.by>
2020-02-18 18:35:19 +0300

    changed urls for whole project add api namespace

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -6,13 +6,16 @@ from django.conf.urls import include
 
 from config.api import api
 
+apipatterns = [
+    path('', include('apps.steps.urls')),
+]
 
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('logout/', logout, {'next_page': '/'}, name='logout'),
     
-    path('api/', include(api.urls)),
+    path('api/', include((apipatterns, 'api'), namespace='api')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     
 ]
