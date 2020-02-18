f0227f5015f822e062bcae0763ad89071ee5b093
SlavaPasedko <mefi-100@tut.by>
2020-02-18 18:34:49 +0300

    add viewset for steps

--- backend/apps/steps/views.py
+++ backend/apps/steps/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .serializers import ProjectStepSerializer
+from .models import ProjectStep
+from rest_framework import viewsets
 
-# Create your views here.
+
+class ProjectStepViewSet(viewsets.ModelViewSet):
+    serializer_class = ProjectStepSerializer
+    queryset = ProjectStep.objects.all() 
+
+
