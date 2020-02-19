168486a75fc8f240243cc03dbcf8be928e89b56a
SlavaPasedko <mefi-100@tut.by>
2020-02-19 18:08:32 +0300

    add button back in header

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -9,27 +9,25 @@
           alt="ML Flow Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.onlinewebfonts.com/svg/img_532570.png"
+          src="@/assets/logo.png"
           transition="scale-transition"
           width="50"
         />
-        <v-img
-          alt="ML Flow Logo"
-          class="shrink mr-2"
-          contain
-          src="https://pngimage.net/wp-content/uploads/2018/06/flow-png-5.png"
-          transition="scale-transition"
-          width="70"
-        />
       </div>
       <v-spacer></v-spacer>
+      <v-btn v-if="!main" to="/" text class="black--text">Back</v-btn>
+      <VerticalDivider
+        class="mx-2"
+        vertical
+        inset
+        color="black"/>
       <v-btn text class="black--text">Sign Up</v-btn>
       <VerticalDivider
         class="mx-2"
         vertical
         inset
         color="black"/>
-      <v-btn text class="black--text">Sign In</v-btn>
+      <v-btn to="/sign-in" text class="black--text">Sign In</v-btn>
     </v-app-bar>
 </template>
 
@@ -40,6 +38,11 @@ export default {
   name: 'Header',
   components: {
       VerticalDivider,
-  }
+  },
+  computed: {
+    main(){
+        return this.$route.path === '/'
+    },
+}
 }
 </script>
