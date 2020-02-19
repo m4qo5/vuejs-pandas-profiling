edf76865596c0efa187cb0a9d87f5f2e4776df70
SlavaPasedko <mefi-100@tut.by>
2020-02-19 18:43:49 +0300

    register auth module in store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import steps from './modules/steps'
+import auth from './modules/auth'
 
 Vue.use(Vuex);
 
@@ -12,6 +13,7 @@ export default new Vuex.Store({
     //mutations,
     //actions,
     modules: {
-        steps
+        steps,
+        auth,
     }
 })
