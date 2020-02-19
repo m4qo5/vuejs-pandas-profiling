f608181b558ac96a303c7af59c1edfde3a71d961
SlavaPasedko <mefi-100@tut.by>
2020-02-19 16:21:01 +0300

    add few components for main page

--- frontend/src/components/MainPageSmallComponents/UsingFlowBlock.vue
+++ frontend/src/components/MainPageSmallComponents/UsingFlowBlock.vue
@@ -1,24 +1,58 @@
 <template>
-  <v-container grid-list-md text-xs-center>
-    <v-layout row wrap v-if="!stepsAreLoading">
-      <v-flex v-for="step in steps" :key="step.id" xs4>
-        <v-hover v-slot:default="{ hover }">
-            <v-card class="mx-auto" max-width="600" shaped>
-            <v-img class="align-top" height="250px" :src="step.picture" gradient="to top right, rgba(255,255,255,.33), rgba(0,100,170,.15)">
-                <v-expand-transition>
-                    <div
-                        v-if="hover"
-                        class="d-flex transition-fast-in-fast-out v-card--reveal font-italic font-weight-light headline black--text"
-                        style="height: 100%;"
-                    >
-                      {{ step.description }}
-                    </div>
-                </v-expand-transition>
-            <v-card-title class="display-2 font-weight-thin">{{ step.title }}</v-card-title>
-            </v-img>
-            </v-card>
-        </v-hover>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <v-timeline :dense="$vuetify.breakpoint.smAndDown">
+    <v-timeline-item
+      color="blue lighten-2"
+      right
+    >
+      <v-card>
+        <v-card-title class="blue lighten-2">
+          <h2 class="display-2 text-center font-weight-thin">Sign Up</h2>
+        </v-card-title>
+      </v-card>
+    </v-timeline-item>
+
+    <v-timeline-item
+      color="blue lighten-2"
+      left
+    >
+      <v-card>
+        <v-card-title class="blue lighten-2 justify-end">
+          <h2 class="display-2 text-center font-weight-thin">Create First Project</h2>
+        </v-card-title>
+      </v-card>
+    </v-timeline-item>
+
+    <v-timeline-item
+      color="blue lighten-2"
+      right
+    >
+      <v-card>
+        <v-card-title class="blue lighten-2">
+          <h2 class="display-2 text-center font-weight-thin">Upload your dataset</h2>
+        </v-card-title>
+      </v-card>
+    </v-timeline-item>
+
+    <v-timeline-item
+      color="blue lighten-2"
+      left
+    >
+      <v-card>
+        <v-card-title class="blue lighten-2 justify-end">
+          <h2 class="display-2 text-center font-weight-thin">Get a report</h2>
+        </v-card-title>
+      </v-card>
+    </v-timeline-item>
+
+    <v-timeline-item
+      color="blue lighten-2"
+      right
+    >
+      <v-card>
+        <v-card-title class="blue lighten-2">
+          <h2 class="display-2 text-center font-weight-thin">Create new versions</h2>
+        </v-card-title>
+      </v-card>
+    </v-timeline-item>
+  </v-timeline>
 </template>
