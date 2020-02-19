0994f64a3ea37cf527dc7bb82831393468fafc24
SlavaPasedko <mefi-100@tut.by>
2020-02-19 18:08:02 +0300

    add SignInView

---
+++ frontend/src/views/SignInView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <SignIn />
+  </div>
+</template>
+
+<script>
+import SignIn from '../components/Signing/SignIn'
+
+export default {
+  name: "SignInView",
+  components: {
+    SignIn, 
+  }
+};
+</script>
