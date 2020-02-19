60eb1adb884eb97b3a1cb256f93f9069cd10f329
SlavaPasedko <mefi-100@tut.by>
2020-02-19 18:07:37 +0300

    add route to SignInView

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home'
+import SignInView from '@/views/SignInView'
 import store from '../store/store'
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ const router =  new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/sign-in',
+      name: 'signin',
+      component: SignInView
     },
 
     { 
