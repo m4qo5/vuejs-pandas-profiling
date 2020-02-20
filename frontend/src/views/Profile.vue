0ffe276c2f58e52e3f1760c3b7d9118adc7568ba
SlavaPasedko <mefi-100@tut.by>
2020-02-20 16:15:30 +0300

    add profile view for router

---
+++ frontend/src/views/Profile.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <Layout />
+  </div>
+</template>
+
+<script>
+import Layout from '../components/Profile/Layout'
+
+export default {
+  name: "Profile",
+  components: {
+    Layout, 
+  }
+};
+</script>
