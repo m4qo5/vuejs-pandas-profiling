3ec0617ccd8753e6eff029b85606af49cb8b3013
SlavaPasedko <mefi-100@tut.by>
2020-02-26 19:21:42 +0300

    some new actions

--- frontend/src/store/modules/projects.js
+++ frontend/src/store/modules/projects.js
@@ -1,21 +1,26 @@
 import {
-    SET_LOADING, SET_PROJECTS, ADD_PROJECT, ADD_PROJECT_TO_CURRENT_PROJECTS, SET_SELECTED_PROJECT
+    SET_LOADING, SET_PROJECTS, ADD_PROJECT, ADD_PROJECT_TO_CURRENT_PROJECTS, SET_SELECTED_PROJECT,
+    ADD_PROJECT_FILE, SET_PROJECT_FILES, SET_IS_SELECTED_PROJECT
 } from '../mutation.types'
 import { http } from '@/services/http'
 
 
 const state = {
     projects: null,
-    projectsAreLoading: null,
+    loading: null,
     newProject: null,
     selectedProject: null,
+    isSelectedProject: false,
+    projectFiles: [],
 }
 
 const getters = {
     projects: state => state.projects,
-    projectsAreLoading: state => state.projectsAreLoading,
+    loading: state => state.loading,
     newProject: state => state.newProject,
     selectedProject: state => state.selectedProject,
+    isSelectedProject: state => state.isSelectedProject,
+    projectFiles: state => state.projectFiles,
 }
 
 
@@ -23,8 +28,11 @@ const mutations = {
     [SET_PROJECTS](state, projects) {
         state.projects = projects
     },
+    [SET_PROJECT_FILES](state, files) {
+        state.projectFiles = files
+    },
     [SET_LOADING](state, payload) {
-        state.projectsAreLoading = payload
+        state.loading = payload
     },
     [ADD_PROJECT](state, payload) {
         state.newProject = payload
@@ -34,6 +42,12 @@ const mutations = {
     },
     [SET_SELECTED_PROJECT](state, payload) {
         state.selectedProject = payload
+    },
+    [SET_IS_SELECTED_PROJECT](state) {
+        state.isSelectedProject = !state.isSelectedProject
+    },
+    [ADD_PROJECT_FILE](state, payload) {
+        state.projectFiles.unshift(payload)
     },
 }
 
@@ -49,12 +63,28 @@ const actions = {
                 commit(SET_LOADING, false)
             })
     },
+    getProjectFiles({ commit }) {
+        commit(SET_LOADING, true)
+        return http.projects.allFiles()
+            .then(data => {
+                commit(SET_PROJECT_FILES, data)
+            })
+            .finally(() => {
+                commit(SET_LOADING, false)
+            })
+    },
     createProject({ commit, dispatch }, payload) {
         return http.projects.create(payload)
             .then(data => {
                 commit(ADD_PROJECT, data)
                 return dispatch('getProjects')
                 //commit(ADD_PROJECT_TO_CURRENT_PROJECTS, data)
+            })
+    },
+    createProjectFile({ commit, dispatch }, payload) {
+        return http.projects.createProjectFile(payload)
+            .then(data => {
+                commit(ADD_PROJECT_FILE, data)
             })
     },
     selectProject({ commit }, payload) {
