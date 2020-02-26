af66dec9ce86ffd21facccc1e7e1b222cf00c55d
SlavaPasedko <mefi-100@tut.by>
2020-02-26 13:59:17 +0300

    add logic for adding files to projects

--- frontend/src/store/modules/projects.js
+++ frontend/src/store/modules/projects.js
@@ -1,5 +1,5 @@
 import {
-    SET_LOADING, SET_PROJECTS, ADD_PROJECT, ADD_PROJECT_TO_CURRENT_PROJECTS
+    SET_LOADING, SET_PROJECTS, ADD_PROJECT, ADD_PROJECT_TO_CURRENT_PROJECTS, SET_SELECTED_PROJECT
 } from '../mutation.types'
 import { http } from '@/services/http'
 
@@ -8,19 +8,20 @@ const state = {
     projects: null,
     projectsAreLoading: null,
     newProject: null,
+    selectedProject: null,
 }
 
 const getters = {
     projects: state => state.projects,
     projectsAreLoading: state => state.projectsAreLoading,
     newProject: state => state.newProject,
+    selectedProject: state => state.selectedProject,
 }
 
 
 const mutations = {
     [SET_PROJECTS](state, projects) {
         state.projects = projects
-
     },
     [SET_LOADING](state, payload) {
         state.projectsAreLoading = payload
@@ -30,6 +31,9 @@ const mutations = {
     },
     [ADD_PROJECT_TO_CURRENT_PROJECTS](state, payload) {
         state.projects = state.projects.push(payload)
+    },
+    [SET_SELECTED_PROJECT](state, payload) {
+        state.selectedProject = payload
     },
 }
 
@@ -49,9 +53,12 @@ const actions = {
         return http.projects.create(payload)
             .then(data => {
                 commit(ADD_PROJECT, data)
-                //return dispatch('getProjects')
-                commit(ADD_PROJECT_TO_CURRENT_PROJECTS, data)
+                return dispatch('getProjects')
+                //commit(ADD_PROJECT_TO_CURRENT_PROJECTS, data)
             })
+    },
+    selectProject({ commit }, payload) {
+        commit(SET_SELECTED_PROJECT, payload)
     },
 }
 
