3ad0f68303bb431c1b1bfcc48c0ab417b570c8ad
SlavaPasedko <mefi-100@tut.by>
2020-02-26 13:58:00 +0300

    add footer

--- frontend/src/components/MainPageAboutBlock.vue
+++ frontend/src/components/MainPageAboutBlock.vue
@@ -4,18 +4,21 @@
         <h1 class="display-2 text-center pt-12 pb-12 font-weight-thin">Data Science is everywhere. We can help you keep your project going on.</h1>
         <WeProvideBlock />
       </v-responsive>
+      <Footer/>
     </v-card>
+    
 </template>
 
 
 <script>
 import WeProvideBlock from './MainPageSmallComponents/WeProvideBlock'
 import VerticalDivider from './MainPageSmallComponents/VerticalDivider'
+import Footer from './MainPageSmallComponents/Footer'
 
   export default {
     name: 'MainPageAboutBlock',
     components : {
-        WeProvideBlock, VerticalDivider
+        WeProvideBlock, VerticalDivider, Footer
     }
 }
 </script>
