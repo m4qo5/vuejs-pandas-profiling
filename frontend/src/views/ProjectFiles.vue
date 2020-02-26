ddee97b7b2035361c8e79085a3e960ae18f95856
SlavaPasedko <mefi-100@tut.by>
2020-02-26 19:22:47 +0300

    rewrite everything to views

---
+++ frontend/src/views/ProjectFiles.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app id="inspire">
+      <Layout />
+      <DrawerList />
+      <SingleProject v-if="isSelectedProject"/>
+  </v-app>
+</template>
+
+<script>
+import Layout from '../components/Profile/Layout'
+import DrawerList from '../components/Profile/DrawerList'
+import SingleProject from '../components/Profile/SingleProject'
+import { mapGetters } from "vuex";
+
+export default {
+  name: "ProjectFiles",
+  components: {
+    Layout, DrawerList, SingleProject
+  },
+  computed: {
+    ...mapGetters(["isSelectedProject"])
+  },
+};
+</script>
---
+++ frontend/src/views/Projects.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app id="inspire">
+      <Layout />
+      <DrawerList/>
+      <Content />
+      <ProjectFileDialog v-if="fileDialog"/>
+      <ProjectDialog v-if="projectDialog"/>
+  </v-app>
+</template>
+
+<script>
+import Layout from '../components/Profile/Layout'
+import DrawerList from '../components/Profile/DrawerList'
+import ProjectDialog from '../components/Profile/ProjectDialog'
+import ProjectFileDialog from '../components/Profile/ProjectFileDialog'
+import Content from '../components/Profile/Content'
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Projects",
+  components: {
+    Layout, DrawerList, ProjectDialog, Content, ProjectFileDialog
+  },
+  computed: {
+    ...mapGetters(["projectDialog", "fileDialog"])
+  },
+};
+</script>
