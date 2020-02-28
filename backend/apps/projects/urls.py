f555ed0b966008afd98ab6b4816769ac8d3ab8a3
SlavaPasedko <mefi-100@tut.by>
2020-02-28 18:03:10 +0300

    add view for report to url

--- backend/apps/projects/urls.py
+++ backend/apps/projects/urls.py
@@ -1,5 +1,6 @@
 from rest_framework import routers
-from .views import (ProjectViewSet, ProjectFileViewSet)
+from .views import (ProjectViewSet, ProjectFileViewSet, get_file_data)
+from django.urls import path, include
 
 
 router = routers.DefaultRouter()
@@ -7,4 +8,6 @@ router = routers.DefaultRouter()
 router.register(r'projects', ProjectViewSet, base_name='projects')
 router.register(r'project-files', ProjectFileViewSet, base_name='project-files')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('file-report', get_file_data, name='file-report'),
+] + router.urls
