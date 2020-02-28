a77b3e845d97658a6730eaad12ac81162633cda4
SlavaPasedko <mefi-100@tut.by>
2020-02-28 13:50:59 +0300

    add fetching on load page

--- frontend/src/components/Profile/SingleProject.vue
+++ frontend/src/components/Profile/SingleProject.vue
@@ -2,7 +2,7 @@
     <v-row justify="center" v-if="!loading">
         <v-expansion-panels accordion>
             <v-expansion-panel
-                v-for="file in filesByProject"
+                v-for="file in selectedProject.project_files"
                 :key="file.id"
             >
                 <v-expansion-panel-header class="display-1 font-weight-thin">{{ file.description }}</v-expansion-panel-header>
@@ -21,11 +21,10 @@ import { mapGetters } from "vuex";
 export default {
     name: "SingleProject",
     computed: {
-    ...mapGetters(["loading", "selectedProject", "projectFiles"]),
-    filesByProject() {
-        let arr = this.projectFiles;
-        return arr.filter(file => file.project == this.selectedProject.id);
-    }   
+    ...mapGetters(["loading", "selectedProject", "isSelectedProject"]),
+    },
+    created() {
+        this.$store.dispatch("selectProject", this.$route.params.id);
     }
 }
 </script>
