2e511aba9ca324e0f0b87ed50ca5e05008a5076c
SlavaPasedko <mefi-100@tut.by>
2020-03-03 18:50:02 +0300

    add styles for iFrame

--- frontend/src/components/Profile/SingleProject.vue
+++ frontend/src/components/Profile/SingleProject.vue
@@ -2,17 +2,18 @@
     <div>
         <v-expansion-panels>
             <v-expansion-panel
+            multiple
             v-for="file in projectFiles"
             :key="file.id"
             >
                 <v-expansion-panel-header class="display-1 font-weight-thin">{{file.file_name}}</v-expansion-panel-header>
-                <v-expansion-panel-content>
+                <v-expansion-panel-content class="content">
                     Description: {{file.description}} <br/> Size: {{file.filesize}}
                     <div v-if="file.report">
                         <v-card-text class="font-weight-thin display-1 justify-center" center>
                             Generated report
                         </v-card-text>
-                        <iframe width="100%" :src="file.report" height="100%">
+                        <iframe frameborder="0" seamless align="baseline" class="frameReport" width="100%" :src="file.report" height="100%">
                         </iframe>
                     </div>
                     <ReportDialog :file_id="file.id" v-else/>
@@ -30,6 +31,11 @@ import ReportDialog from './ReportDialog'
 export default {
     name: "SingleProject",
     components: {ReportDialog},
+    data () {
+        return { 
+
+        }
+    },
     computed: {
     ...mapGetters(["loading", "selectedProject", "reportDialog"]),
     projectFiles() {
@@ -38,6 +44,7 @@ export default {
     },
     mounted: function() {
         this.$store.dispatch("selectProject", this.$route.params.id);
+        this.$store.dispatch("getProjects");
     },
     methods: {
       generateReport(value) {
@@ -49,8 +56,15 @@ export default {
 
 
 <style scoped>
-iframe {
-    width: 1000px;
-    height: 1000px;
+.frameReport {
+    position: relative;
+    width: 100%;
+    height: 800px;
 }
+
+.content {
+    min-height: 100%
+}
+
+
 </style>
